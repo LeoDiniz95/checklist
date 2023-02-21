40dc4c787cfcd5479a57215c761e020055868244
leodiniz <leonardo.diniz95@yahoo.com.br>
2023-02-21 17:02:31 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace checklist_api.Models
+{
+    [Table("todolist")]
+    public class CheckList
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public decimal done { get; set; }
+
+        public string status { get; set; }
+    }
+}
